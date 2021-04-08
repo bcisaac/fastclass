13b1d777d09a13ef09b066d846ae23a3d879bd62
bcisaac <ibconner01@gmail.com>
2021-04-08 01:32:22 -0500

    Changed text, font, and colors

--- App.js
+++ App.js
@@ -5,8 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Text style={styles.textStyle}>Welcome to my app!</Text>
     </View>
   );
 }
@@ -14,8 +13,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#EEEDE7',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle: {
+    fontFamily: "fantasy",
+    color: '#868B8E',
+    fontSize:32,
+  }
 });
